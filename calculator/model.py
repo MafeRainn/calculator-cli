688e3a51d78092682736f1eb0b1dcb13bf2f8f3b
Mafe A <maguirre065@soyudemedellin.edu.co>
2024-07-30 13:35:52 -0500

    añadi el resto de metodos

--- calculator/model.py
+++ calculator/model.py
@@ -1,5 +1,12 @@
 class Calculator:
     def add(self, num1:float, num2:float) -> float:
         return num1 + num2
-    
-    
+    def substract(self, num1:float, num2:float) -> float:
+        return num1 - num2
+    def multiply(self, num1:float, num2:float) -> float:
+        return num1 * num2
+    def divide(self, num1:float, num2:float) -> float:
+        if num2 == 0:
+            return "cannot divide by 0!"
+        else:
+            return num1 / num2
